ab6d19b8c8ab70a34ca426fac0fe6b1b005989b6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-10-27 22:43:24 -0300

    inicia programa

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿int numero;
+
+do
+{
+    Console.WriteLine
+        ("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    int34.TryParse(Console.ReadLine());
+}
+while (numero <= 0);
+
+if (numero = 0)
+{
+    Console.WriteLine("Operação Cancelada.\n Volte Sempre!");
+}
+else
+{
+    numero = 1 >= 9
+    Console.WriteLine("Operação Cancelada.\n Volte Sempre!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://gist.github.com/diegoneri/e86a3a6bc988a5934e194d5ebe134e49
