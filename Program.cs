808c90f9f4e16caacc791cca70ffc786b438d6b9
Nicole <rosanik3103@gmail.com>
2023-11-24 20:37:02 -0300

    acrescenta programa

--- Program.cs
+++ Program.cs
@@ -1,45 +1,17 @@
-﻿int numero;
+﻿int numero = 0;
+
+Console.WriteLine
+    ("--- Entrada Válida ---");
 
 do
 {
-    Console.WriteLine
+    Console.Write
         ("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
-    int34.TryParse(Console.ReadLine());
+    Console.ReadLine();
 }
-while (numero <= 0);
+while (numero < 0);
 
-if (numero = 0)
+if (numero == 0)
 {
-    Console.WriteLine("Operação Cancelada.\n Volte Sempre!");
+    Console.WriteLine("Operação Cancelada.\nVolte Sempre!");
 }
-else
-{
-    numero = 1 >= 9
-    Console.WriteLine("Operação Cancelada.\n Volte Sempre!");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//https://gist.github.com/diegoneri/e86a3a6bc988a5934e194d5ebe134e49
